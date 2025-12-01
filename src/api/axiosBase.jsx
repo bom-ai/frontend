ef4ecb1defab078cd9da89ed334c9c1730b55c65
ahnji0520 <ahnji020520@gmail.com>
2025-12-01 19:53:15 +0900

    cookie setting

--- src/api/axiosBase.jsx
+++ src/api/axiosBase.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 const axiosBase = axios.create({
   baseURL: "https://bomatic-server-ymu7mz7dqa-du.a.run.app/api",
   headers: { "Content-Type": "application/json" },
+  withCredentials: true,
 });
 
 export default axiosBase;
